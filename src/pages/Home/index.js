23455d1f3aa900d9695fa89702e126bbda2f8bba
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-17 08:30:44 -0300

    Add Post component and rendering them using posts.map.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
-import Banner from "components/Banner"
+import Banner from "components/Banner";
+import styles from "./Home.module.css";
+import posts from "json/posts.json"
+import Post from "components/Post";
 
 const Home = () => {
   return (
     <main>
       <Banner/>
-      <h1>Home</h1>
+
+      <ul className={styles.posts}>
+        {posts.map((post) =>
+          <li key={post.id}>
+            <Post post={post}/>
+          </li>
+        )}
+      </ul>
     </main>
   )
 }
